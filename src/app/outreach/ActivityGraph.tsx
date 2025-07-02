3ddaa0552d2d0f7dfdd491e6bed6ef36257bb47e
SlushEE0 <neeladem2009@gmail.com>
2025-07-01 19:55:07 -0700

    removed: area chart fill attempts

--- src/app/outreach/ActivityGraph.tsx
+++ src/app/outreach/ActivityGraph.tsx
@@ -187,16 +187,6 @@ export default function ActivityGraph({ id }: OutreachActivityGraphProps) {
       <ResponsiveContainer width="100%" height="100%">
         <AreaChart data={activityData}>
           <Tooltip content={<CustomTooltip />} />
-          <defs>
-            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-            </linearGradient>
-            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-            </linearGradient>
-          </defs>
           <Area
             type="monotone"
             dataKey="events"
