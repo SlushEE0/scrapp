8a3fa72fba6612ffa418f624f67434a6c369908b
SlushEE0 <neeladem2009@gmail.com>
2025-08-24 15:43:24 -0700

    PWA functionality

---
+++ src/app/manifest.ts
@@ -0,0 +1,20 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Scrapp",
+    short_name: "Scrapp",
+    description: "Your buddy for better disposal",
+    start_url: "/",
+    display: "minimal-ui",
+    background_color: "#d0f1c5",
+    theme_color: "#19573f",
+    icons: [
+      {
+        src: "/favicon.png",
+        sizes: "512x512",
+        type: "image/png"
+      }
+    ]
+  };
+}
